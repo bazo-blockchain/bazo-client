9bc86f231862013554e36546a71bdf3eef98d9e2
KuersatAydinli <kuersat.aydinli@uzh.ch>
2019-02-23 09:10:02 +0100

    further MPT cleanup

--- cstorage/read.go
+++ cstorage/read.go
@@ -37,19 +37,3 @@ func ReadLastBlockHeader() (header *protocol.Block) {
 
 	return header
 }
-
-func ReadMptProofs() (proof *protocol.MPT_Proof, err error) {
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(MERKLEPATRICIAPROOF_BUCKET))
-		k, _ := b.Cursor().First()
-		encdoedProof := b.Get(k)
-		proof = proof.Decode(encdoedProof)
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return proof, nil
-}
