49c3923ead2b9846e9007e12aa75f1de877c1ada
mchetelat <)7BWmeDNuakxj*g@>
2019-05-06 21:48:15 +0200

    Bugfixes

--- cstorage/read.go
+++ cstorage/read.go
@@ -9,7 +9,7 @@ func ReadBlockHeader(hash [32]byte) (header *protocol.Block) {
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("blockheaders"))
 		encodedHeader := b.Get(hash[:])
-		header = header.Decode(encodedHeader)
+		header = header.DecodeHeader(encodedHeader)
 
 		return nil
 	})
@@ -26,7 +26,7 @@ func ReadLastBlockHeader() (header *protocol.Block) {
 		b := tx.Bucket([]byte("lastblockheader"))
 		cb := b.Cursor()
 		_, encodedHeader := cb.First()
-		header = header.Decode(encodedHeader)
+		header = header.DecodeHeader(encodedHeader)
 
 		return nil
 	})
